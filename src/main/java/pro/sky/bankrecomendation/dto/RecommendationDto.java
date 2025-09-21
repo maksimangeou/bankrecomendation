2b670f12861e5f3a8a6140c41a57ebfe4266c86e
maksimangeou <ange_fuck@icloud.com>
2025-09-21 17:23:19 +0300

    (4)

--- src/main/java/pro/sky/bankrecomendation/dto/RecommendationDto.java
+++ src/main/java/pro/sky/bankrecomendation/dto/RecommendationDto.java
@@ -17,28 +17,4 @@ public class RecommendationDto {
     private String name;
     private String text;
 
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
